e3108279e84f278de3c62631313a561f0a68f394
jorge <jabecerra@uc.cl>
2020-09-08 15:14:49 -0300

    more navigation

--- src/HomeStack.jsx
+++ src/HomeStack.jsx
@@ -1,6 +1,6 @@
 // 58:04
 
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Center } from './Center';
 import { TouchableOpacity, Text, FlatList, Button } from 'react-native';
@@ -32,10 +32,41 @@ const Feed = ({ navigation }) => {
   );
 };
 
-const Product = ({ route }) => {
+const Product = ({ route, navigation }) => {
   return (
     <Center>
-      <Text> {route.params.name}</Text>
+      <Button
+        title={`Edit ${route.params.name}`}
+        onPress={() =>
+          navigation.navigate('EditProduct', { name: route.params.name })
+        }
+      />
+    </Center>
+  );
+};
+
+const apiCall = (x) => {
+  console.log('form submitted');
+  return x;
+};
+
+const EditProduct = ({ route, navigation }) => {
+  const [formState] = useState();
+  const submit = useRef();
+
+  submit.current = () => {
+    // api call with new form state
+    apiCall(formState);
+    navigation.goBack();
+  };
+
+  useEffect(() => {
+    navigation.setParams({ submit });
+  }, []);
+
+  return (
+    <Center>
+      <Text> Editing {route.params.name}</Text>
     </Center>
   );
 };
@@ -51,7 +82,10 @@ const HomeStack = () => {
         options={{
           headerRight: () => {
             return (
-              <TouchableOpacity onPress={() => logout()}>
+              <TouchableOpacity
+                onPress={() => logout()}
+                style={{ paddingRight: 8 }}
+              >
                 <Text>Log out</Text>
               </TouchableOpacity>
             );
@@ -64,6 +98,26 @@ const HomeStack = () => {
         options={({ route }) => {
           return {
             headerTitle: `Product: ${route.params.name}`,
+          };
+        }}
+      />
+      <Stack.Screen
+        name="EditProduct"
+        component={EditProduct}
+        options={({ route }) => {
+          return {
+            headerTitle: `Edit ${route.params.name}`,
+            headerRight: () => (
+              <TouchableOpacity
+                style={{ paddingRight: 8 }}
+                onPress={() => {
+                  // submit form
+                  route.params.submit.current();
+                }}
+              >
+                <Text style={{ color: 'red' }}>Done</Text>
+              </TouchableOpacity>
+            ),
           };
         }}
       />
